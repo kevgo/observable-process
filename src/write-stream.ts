b5da51fb03780387403aecc89a4c2655bee011cb
Kevin Goslar <kevin.goslar@gmail.com>
2019-05-21 17:59:16 -0400

    Fix comment (#75)

--- src/write-stream.ts
+++ src/write-stream.ts
@@ -1,5 +1,5 @@
-// a stream that we can write into using write(string),
-// plus some boilerplate to make process.stdout fit in here
+// A stream that we can write into using write(string),
+// plus some boilerplate to make process.stdout fit in here.
 export interface WriteStream {
   write(
     chunk: Buffer | string,

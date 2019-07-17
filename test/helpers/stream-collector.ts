64767a3ae9919c15ef7d494646136107ca7931a7
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-18 05:51:51 +0800

    progress

---
+++ test/helpers/stream-collector.ts
@@ -0,0 +1,2 @@
+/** Collects */
+export class StreamCollector {}
--- test/verbose-test.ts
+++ test/verbose-test.ts
@@ -3,7 +3,7 @@ import { ObservableProcess } from '../src/observable-process'
 
 describe('verbose mode', function() {
   it('adds information about the process health to the error stream', async function() {
-    let stdErrContent = ''
+    let stdErrContent = '' // this variable contains the STDERR output
     const process = new ObservableProcess({
       commands: [
         'node',
@@ -21,5 +21,26 @@ describe('verbose mode', function() {
     await process.waitForEnd()
     assert.ok(stdErrContent.includes('PROCESS ENDED'))
     assert.ok(stdErrContent.includes('EXIT CODE: 0'))
+  })
+
+  it('does not add information when disabled', async function() {
+    let stdErrContent = '' // this variable contains the STDERR output
+    const process = new ObservableProcess({
+      commands: [
+        'node',
+        '-e',
+        "console.log('normal output'); console.error('error output')"
+      ],
+      verbose: false,
+      stderr: {
+        write: (text: string) => {
+          stdErrContent += text
+          return false
+        }
+      }
+    })
+    await process.waitForEnd()
+    assert.ok(!stdErrContent.includes('PROCESS ENDED'))
+    assert.ok(!stdErrContent.includes('EXIT CODE: 0'))
   })
 })

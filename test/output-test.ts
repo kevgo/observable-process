99af73ecd83fd30cfebb943558aee8afdb102115
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-18 08:10:38 +0800

    convert output

---
+++ test/output-test.ts
@@ -0,0 +1,34 @@
+import { ObservableProcess } from '../dist/observable-process'
+import { strict as assert } from 'assert'
+
+describe('output', function() {
+  it('pipes the output to the provided custom streams', async function() {
+    // run a process that prints to both STDOUT and STDERR
+    let stdOutText = ''
+    let stdErrText = ''
+    this.process = new ObservableProcess({
+      commands: [
+        'node',
+        '-e',
+        "console.log('normal output'); console.error('error output')"
+      ],
+      stdout: {
+        write: (text: string) => {
+          stdOutText += text
+          return false
+        }
+      },
+      stderr: {
+        write: text => {
+          stdErrText += text
+          return false
+        }
+      }
+    })
+    await this.process.waitForEnd()
+
+    // verify the piped output
+    assert.equal(stdOutText, 'normal output\n')
+    assert.equal(stdErrText, 'error output\n')
+  })
+})

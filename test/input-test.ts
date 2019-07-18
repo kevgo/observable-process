283a1ad83cac1242df23bc6e8b7f560e2a9aaef0
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-18 10:22:25 +0800

    io

--- test/input-test.ts
+++ test/input-test.ts
@@ -1,15 +1,6 @@
 import { strict as assert } from 'assert'
 import { startProcess } from './helpers/start-process'
 
-describe('.fullOutput()', function() {
-  it('returns the full output received from the process', async function() {
-    const process = startProcess('console.log("hello")')
-    await process.waitForText('hello')
-    const output = process.fullOutput()
-    assert.equal(output, 'hello\n')
-  })
-})
-
 describe('STDIN', function() {
   it('allows entering text into the running process', async function() {
     // start a process that reads from STDIN
--- test/output-test.ts
+++ test/output-test.ts
@@ -1,5 +1,15 @@
 import { ObservableProcess } from '../dist/observable-process'
 import { strict as assert } from 'assert'
+import { startProcess } from './helpers/start-process'
+
+describe('.fullOutput()', function() {
+  it('returns the full output received from the process', async function() {
+    const process = startProcess('console.log("hello")')
+    await process.waitForText('hello')
+    const output = process.fullOutput()
+    assert.equal(output, 'hello\n')
+  })
+})
 
 describe('output', function() {
   it('provides the accumulated output')

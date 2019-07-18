7893ef36af03ab92f9aa3997829fb37b9b8fed73
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-18 11:27:01 +0800

    finalize

--- features/example-apps/console-output.js
+++
@@ -1,5 +0,0 @@
-// This is an example app used for testing.
-// It output text on both the stdout and stderr streams.
-
-console.log("normal output")
-console.error("error output")
--- test/start-test.ts
+++ test/start-test.ts
@@ -1,17 +1,39 @@
-import { ObservableProcess } from '../src/observable-process'
+import { ObservableProcess } from "../src/observable-process"
+import { strict as assert } from "assert"
 
-describe('constructor', function() {
-  it('allows to start a process via an array of arguments', async function() {
+describe("constructor", function() {
+  it("allows to start a process via an array of arguments", async function() {
     const process = new ObservableProcess({
-      commands: ['node', '-e', "console.log('hello')"]
+      commands: ["node", "-e", "console.log('hello')"]
     })
-    await process.waitForText('hello')
+    await process.waitForText("hello")
   })
 
-  it('allows to start a process via a string', async function() {
+  it("allows to start a process via a string", async function() {
     const process = new ObservableProcess({
       command: "node -e console.log('hello')"
     })
-    await process.waitForText('hello')
+    await process.waitForText("hello")
+  })
+
+  it("starts global processes that are in the path", async function() {
+    const process = new ObservableProcess({
+      command: "node -h"
+    })
+    await process.waitForEnd()
+    assert.equal(process.exitCode, 0)
+  })
+
+  it("starts local processes in the current directory")
+})
+
+describe("environment variables", function() {
+  it("allows to provide custom environment variables for running processes", async function() {
+    const process = new ObservableProcess({
+      commands: ["node", "-e", "console.log('foo:', process.env.foo)"],
+      env: { foo: "bar" }
+    })
+    await process.waitForEnd()
+    assert.equal(process.fullOutput(), "foo: bar\n")
   })
 })

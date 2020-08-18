ec2478c4bc43f031afcc98ee78b4ac6d6fd7f111
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-18 07:32:02 -0500

    Test exit codes (#108)

--- test/wait-for-end-test.ts
+++ test/wait-for-end-test.ts
@@ -6,10 +6,10 @@ const delay = util.promisify(setTimeout)
 suite("ObservableProcess.waitForEnd()")
 
 test("process ends before calling it", async function () {
-  const observable = start(["node", "-e", "console.log('hello')"])
+  const observable = start(["node", "-e", "console.log('hello'); process.exit(7)"])
   await delay(50)
   const result = await observable.waitForEnd()
-  assert.deepEqual(result.exitCode, 0)
+  assert.deepEqual(result.exitCode, 7)
   assert.deepEqual(result.killed, false)
   assert.deepEqual(result.stdOutput, "hello\n")
   assert.deepEqual(result.errOutput, "")
@@ -17,9 +17,9 @@ test("process ends before calling it", async function () {
 })
 
 test("process still running when calling it", async function () {
-  const observable = start(["node", "-e", "setTimeout(function() { console.log('finally')}, 10)"])
+  const observable = start(["node", "-e", "setTimeout(function() { console.log('finally'); process.exit(8)}, 10)"])
   const result = await observable.waitForEnd()
-  assert.equal(result.exitCode, 0)
+  assert.equal(result.exitCode, 8)
   assert.equal(result.killed, false)
   assert.equal(result.stdOutput, "finally\n")
   assert.equal(result.errOutput, "")

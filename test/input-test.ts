fb0436a5147c5e1e595c3a5a0344577d0dc4a518
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-18 07:31:12 -0500

    Test multiple writes

--- test/input-test.ts
+++ test/input-test.ts
@@ -4,17 +4,19 @@ import { startNodeProcess } from "./helpers/start-node-process"
 test("ObservableProcess.stdin", async function () {
   // start a process that reads from STDIN
   const running = startNodeProcess(
-    "process.stdin.on('data', data => { process.stdout.write(data.toString()) });\
+    "process.stdin.on('data', data => { process.stdout.write(data) });\
        process.stdin.on('end', () => { process.stdout.write('\\nEND') })"
   )
 
   // write some stuff into the STDIN stream of this process
   running.stdin.write("hello")
+  running.stdin.write(" ")
+  running.stdin.write("world")
 
   // close the STDIN stream
   running.stdin.end()
 
   // verify
   await running.waitForEnd()
-  assert.equal(running.output.fullText(), "hello\nEND")
+  assert.equal(running.output.fullText(), "hello world\nEND")
 })

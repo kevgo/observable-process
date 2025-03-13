7ee37cff0cc68a964a6fb9a34f523edfb7e1bebb
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-13 10:41:54 -0500

    Update ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+
+import tslintPlugin from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+
+export default [
+  {
+    files: ["src/*.ts"],
+    ignores: ["**/node_modules/", ".git/", "dist/", "**/dist/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: {
+        console: "readonly",
+        module: "readonly",
+        process: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tslintPlugin,
+    },
+    rules: {
+      ...tslintPlugin.configs.recommended.rules,
+      "no-empty-function": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-call": "error",
+    },
+  },
+]
